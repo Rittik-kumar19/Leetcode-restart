89c9e98bc7f05de602a6e1d86265350b4de24876
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2024-03-04 01:30:14 +0530

    Time: 3 ms (64.52%) | Memory: 13.2 MB (46.98%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -1,24 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if(head->next == NULL) return NULL;
-        
-        ListNode* slow = head;
-        ListNode* fast = head;
-        
-        while(n != 0) {
-            fast = fast->next;
-            n--;
+        if(head == nullptr){
+            return head;
         }
+        ListNode *temp = head;
+        int cnt = 0;
         
-        while(fast != NULL && fast->next != NULL) {
-            fast = fast->next;
-            slow = slow->next;
+        while(temp!=nullptr){
+            cnt++;
+            temp=temp->next;
         }
-        
-        if(slow == head && fast == NULL) return head->next;
-        
-        slow->next = slow->next->next;
+        cout<<cnt<<endl;
+        if(n== cnt){
+           ListNode *newhead = head -> next;
+            head -> next = nullptr;
+            // delete(head);
+            return newhead;
+        }
+        int node_index_to_remove = cnt - n;
+        int i = 0;
+        ListNode *curr = head;
+        ListNode *prev = head;
+        while(i!=node_index_to_remove){
+            prev = curr;
+            curr = curr->next;
+            i++;
+        }
+        prev->next = curr->next;
+        // delete(curr);
         return head;
+        
+        // return temp;
     }
 };
