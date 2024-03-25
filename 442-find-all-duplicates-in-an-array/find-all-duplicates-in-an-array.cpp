ae3faaf27c23ad47d6c3f04518faf16c2ef4e2ca
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2024-03-26 03:12:55 +0530

    Time: 30 ms (95.98%) | Memory: 35.8 MB (98.04%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -1,3 +1,25 @@
+// class Solution {
+// public:
+//     vector<int> findDuplicates(vector<int>& nums) {
+//         // Approach - 1. - map 
+//         // Approach - 2 -  using sorting & iteration
+//         // Approach - 3 - using negative marking
+//         vector<int> ans;
+//         nums[nums[0] - 1] *=-1;
+//         for(int i=1;i<nums.size();i++){
+//             int index = abs(nums[i]);
+//             if(nums[index-1]<0){
+//                 ans.push_back(abs(nums[index-1]));
+//             }else{
+//                 nums[index-1] *=-1;
+//             }
+//         }
+//         sort(ans.begin(), ans.end());
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
