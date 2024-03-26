c376bdcfdad35a5acc11adf3d97bde7518e6e066
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2024-03-27 03:32:18 +0530

    Time: 40 ms (74.87%) | Memory: 43.6 MB (55.17%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,13 +1,39 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        int n=nums.size();
-        for(int i=0 ; i<n ; i++)
-        {
-            while(nums[i]>0 and nums[i]<=n and nums[i]!=nums[nums[i]-1])
-                swap(nums[i],nums[nums[i]-1]);
+        // approach 1 - [1, 2, 3,4,5] & without sorting
+        // approach 2 - [1, 3, 6, 7, 2] => you can use set or freq map of size [N+1]
+        // approach 3 - [1, 3, 5, 6,4] -> using negative sign conversion
+        
+        bool is1present = false;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] == 1){
+                is1present = true;
+            }
+            
+            if(nums[i]<=0 || nums[i]>nums.size()){
+                nums[i] = 1;
+            }
         }
-        for(int i=0 ; i<n ; i++) if(nums[i] != i+1) return i+1;
-        return n+1;
+        if(!is1present){
+            return 1;
+        }
+        
+        for(int i=0;i<nums.size();i++){
+            int num = abs(nums[i]);
+            int idx = num - 1;
+            
+            if(nums[idx]<0) continue;
+            
+            nums[idx] *= -1;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>0){
+                return i+1;
+            }
+        }
+        
+        return nums.size()+1;
+    
     }
 };
