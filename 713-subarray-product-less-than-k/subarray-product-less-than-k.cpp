00a955d674d08d38f4a5363f25fa2e582ef1a3c8
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2024-03-29 00:26:18 +0530

    Time: 55 ms (68.93%) | Memory: 63.8 MB (13.88%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    int numSubarrayProductLessThanK(vector<int>& v, int k) {
-        int ans = 1 , sum = 0 ;
-        int ind1 = 0  , ind2 = 0;
-        while(ind2 < v.size() and ind1 < v.size()){
-            ans *= v[ind2] ;
-            if(ans >= k){
-                int cnt = ind2-ind1 ;
-                sum += (cnt*(cnt+1)/2) ;
-                int cnt2 =0 ;
-                while(ind1 <= ind2){
-                    if(ans < k) break ;
-                    ans /= v[ind1] ;
-                    ind1++; cnt2++ ;
-                }
-                cnt = max(0 , cnt-cnt2);
-                sum -= (cnt*(cnt+1)/2) ;
-            }
-            ind2++;
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        //Classic template of sliding windows - khandaani template
+        int n = nums.size();
+        if(k<=1){
+            return 0;
         }
-        // cout << ind1 << ind2 << endl ;
-        int cnt = ind2-ind1 ;
-        sum += (cnt*(cnt+1)/2) ;
-        return sum ;
+        int i = 0, j=0;
+        int count = 0, prod = 1;
+        while(j<n){
+            prod *= nums[j];
+            while(prod >= k){
+                prod /= nums[i];
+                i++;
+            }
+            
+            count += (j-i + 1);
+            j++;
+        }
+        return  count;
     }
 };
